893283b71cc86dee97faeeaa6a10abbe36d8200a
PrajwalDeshmani <156641275+PrajwalDeshmani@users.noreply.github.com>
2026-04-22 17:17:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,5 @@
-
+variable "project_id" {}
+variable "primary_region" {}
+variable "secondary_region" {}
+variable "zones_primary" { type = list(string) }
+variable "zones_secondary" { type = list(string) }
