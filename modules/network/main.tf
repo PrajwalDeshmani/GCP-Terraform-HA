e58361129bea42cab12c1a5863b9f5bd9f748d1f
PrajwalDeshmani <156641275+PrajwalDeshmani@users.noreply.github.com>
2026-04-22 17:19:08 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,1 +1,18 @@
+resource "google_compute_network" "vpc" {
+  name                    = "ha-vpc"
+  auto_create_subnetworks = false
+}
 
+resource "google_compute_subnetwork" "subnet1" {
+  name          = "subnet-primary"
+  ip_cidr_range = "10.10.0.0/16"
+  region        = "asia-south1"
+  network       = google_compute_network.vpc.id
+}
+
+resource "google_compute_subnetwork" "subnet2" {
+  name          = "subnet-secondary"
+  ip_cidr_range = "10.20.0.0/16"
+  region        = "asia-east1"
+  network       = google_compute_network.vpc.id
+}
