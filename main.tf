186e1d20888e838a1fe91e46225d7baedefa8878
PrajwalDeshmani <156641275+PrajwalDeshmani@users.noreply.github.com>
2026-04-22 17:18:41 +0530

    Add Terraform modules for network and compute resources

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
+module "network" {
+  source = "./modules/network"
 
+  project_id = var.project_id
+}
+
+module "compute_primary" {
+  source = "./modules/compute"
+
+  region = var.primary_region
+  zones  = var.zones_primary
+  name   = "primary"
+  network = module.network.network_self_link
+}
+
+module "compute_secondary" {
+  source = "./modules/compute"
+
+  region = var.secondary_region
+  zones  = var.zones_secondary
+  name   = "secondary"
+  network = module.network.network_self_link
+}
+
+module "loadbalancer" {
+  source = "./modules/loadbalancer"
+
+  instance_group_1 = module.compute_primary.instance_group
+  instance_group_2 = module.compute_secondary.instance_group
+}
